fb85ef28d2ae48f8dad26f7be6919b860748ba5a
SrH4ck <antonio.iesgerena@gmail.com>
2024-10-03 14:25:18 +0200

    03/10/24
    03/10/24

---
+++ Desarrollo de interfaces/ConsoleAppTipoDatos/ConsoleAppTipoDatos/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//DATOS PRIMITIVOS
+//----------------
+
+// Variable enteras
+int varEntero01 = 33;
+int varEntero02 = 2;
+int varEntero03;
+varEntero03 = varEntero01 + varEntero02;
+Console.WriteLine(varEntero03);
+
+byte varEntero04 = 222;
+varEntero03 = varEntero04;
+varEntero04 = (byte)varEntero01;
+
+// Variables decimales
+float varDecimalSimple01 = 4.8f;
+float varDecimalSimple02 = 15.16f;
+Console.WriteLine(varDecimalSimple01 * varDecimalSimple02);
+
+double varDecimaDoble01 = 23.42;
+Console.WriteLine(varDecimaDoble01 * varDecimalSimple01);
+
+varDecimalSimple01 = (float) varDecimaDoble01;
+varDecimaDoble01 = varDecimalSimple01;
+
+// Varaibles Booleanas
+bool varBoolean01 = false;
+bool varBoolean02 = true;
+Console.WriteLine(varBoolean02);
+Console.WriteLine(varBoolean01);
+Console.WriteLine(varBoolean02 || varBoolean01);
+
+//Variables Cadenas de Caracteres
+string varString01 = "Hola";
+string varString02 = "Mundo";
+Console.WriteLine(varString01 + varString02);
+
+char varChar01 = ' ';
+Console.WriteLine(varString01 + varChar01 + varString02);
+
+//Variables Enumeradas
+enum ColoresRGB
+{
+    Rojo,
+    Verde,
+    Azul
+}
+
+enum Pad
+{
+    Almeria,
+    Cadiz,
+    Cordoba,
+    Granada,
+    Huelva,
+    Jaen,
+    Malaga,
+    Sevilla
+}
+
+for(Pad provincia = Pad.Almeria; provincia <= Pad.Sevilla; provincia++)
+{
+
+}
---
+++ Desarrollo de interfaces/ConsoleEnum/ConsoleEnum/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace ConsoleEnum
+{
+    internal class Program
+    {
+        enum ColoresRGB
+        {
+            Rojo,
+            Verde,
+            Azul
+        }
+        enum ProvinciaAnd
+        {
+            Almeria = 4,
+            Cadiz = 11,
+            Cordoba = 14,
+            Granada = 18,
+            Huelva = 21,
+            Jaen = 23,
+            Malaga = 29,
+            Sevilla = 41
+        }
+
+
+        struct Ficha
+        {
+            string nombre;
+            public string apellidos { set; get; }
+            public int edad { get; set; }
+
+
+            
+            public Ficha(string nombre, string apellido, int edad)
+            {
+                this.nombre = nombre;  
+                this.apellidos = apellido;
+                this.edad = edad;
+            }
+
+            public override string ToString()
+            {
+                return nombre + " " + apellidos + " (" + edad + ")";
+            }
+
+            public string getNombre()
+            {
+                return nombre;
+            }
+            public void setNombre (string nombre)
+            {
+                this.nombre = nombre;
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            for (ProvinciaAnd provincia = ProvinciaAnd.Almeria; provincia <= ProvinciaAnd.Sevilla; provincia++)
+            {
+                Console.WriteLine((int)provincia +" " + provincia);
+            }
+
+            foreach(ProvinciaAnd provincias in Enum.GetValues(typeof(ProvinciaAnd)))
+            {
+                Console.WriteLine((int) provincias + " " + provincias.ToString());
+            }
+
+            //Estructura
+            Ficha fichaAlum;
+            fichaAlum = new Ficha();
+            fichaAlum.setNombre("Antonio");
+            fichaAlum.apellidos = "Mesa Sánchez";
+            fichaAlum.edad = 27;
+            Console.WriteLine(fichaAlum);
+            Console.WriteLine(fichaAlum.getNombre() + fichaAlum.apellidos + fichaAlum.edad);
+            Console.WriteLine(fichaAlum.edad);
+
+            Ficha fichaAlum2 = new Ficha("Antonio", "Mesa Sánchez" , 27);
+            Console.WriteLine(fichaAlum2);
+            Console.WriteLine(fichaAlum2.getNombre();
+        }
+    }
+}
